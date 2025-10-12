33baef3e443c4948c99aaeab049486b4dcb5b3b6
MacBookPro <alex@gmail.com>
2025-10-12 16:15:42 +0300

    Отредактировал main.py, добавил API ключик в переменную окружения, вроде работает.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 import asyncio
-from config import TELEGRAM_TOKEN
+import os
 
 from telebot.async_telebot import AsyncTeleBot
 
-bot = AsyncTeleBot(TELEGRAM_TOKEN)
+bot = AsyncTeleBot(os.environ["TELEGRAM_BOT"])
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
